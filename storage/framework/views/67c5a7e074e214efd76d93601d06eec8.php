039185d95257a7879a9b7375fe4f4c00d3090d82
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-02-26 20:54:14 -0300

    [#54] Seção - Info
    http://localhost:8080/work_packages/54

--- storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
+++ storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
@@ -58,6 +58,31 @@
                     <img src="<?php echo e(asset("assets/image/client-3.png")); ?>" alt="client 3" class="hover:contrast-200"/>
                 </div>
             </section>
+
+            <section class="">
+                <hr class="max-w-7xl mx-auto mb-64 border-gray-300"/>
+                <div class="flex space-x-8">
+                    <img src="https://picsum.photos/600/400" alt="Site Personalizado e Exclusivo" class="w-1/2" data-aos="fade-right" data-aos-delay="500" data-aos-anchor-placement="center-center"/>
+                    <div class="max-w-3xl space-y-8 pt-16" data-aos="fade-left" data-aos-delay="500" data-aos-anchor-placement="center-center">
+                        <h3 class="text-3xl font-extrabold w-full font-mono">Site Personalizado e Exclusivo</h3>
+                        <p class="text-lg leading-10">Desenvolvemos sites que refletem a identidade da sua marca e atendem às suas necessidades específicas, sem modelos genéricos.</p>
+                    </div>
+                </div>
+                <div class="flex  flex-row-reverse justify-items-end">
+                    <img src="https://picsum.photos/600/400" alt="Site Personalizado e Exclusivo" class="w-1/2" data-aos="fade-left" data-aos-delay="500" data-aos-anchor-placement="center-center"/>
+                    <div class="max-w-3xl space-y-8 pt-16" data-aos="fade-right" data-aos-delay="500" data-aos-anchor-placement="center-center">
+                        <h3 class="text-3xl font-extrabold w-full font-mono">Design Responsivo e Otimizado</h3>
+                        <p class="text-lg leading-10">Seu site será perfeito em qualquer dispositivo (computador, tablet, celular) e otimizado para os motores de busca (Google), garantindo que seus clientes te encontrem facilmente.</p>
+                    </div>
+                </div>
+                <div class="flex space-x-8">
+                    <img src="https://picsum.photos/600/400" alt="Site Personalizado e Exclusivo" class="w-1/2" data-aos="fade-right" data-aos-delay="500" data-aos-anchor-placement="center-center"/>
+                    <div class="max-w-3xl space-y-8 pt-16" data-aos="fade-left" data-aos-delay="500" data-aos-anchor-placement="center-center">
+                        <h3 class="text-3xl font-extrabold w-full font-mono">Soluções Completas e Integradas</h3>
+                        <p class="text-lg leading-10">Desde a criação do layout até a implementação de funcionalidades avançadas, oferecemos um serviço completo para que você não precise se preocupar com nada.</p>
+                    </div>
+                </div>
+            </section>
         </main>
 
         <footer class="border-t border-t-gray-300 py-8 text-center">
