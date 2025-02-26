84e3ae2736d96b866b7c641f5c5e9259e05de833
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-02-26 20:38:28 -0300

    [#52] Seção - Hero
    http://localhost:8080/work_packages/52

--- storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
+++ storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
@@ -37,6 +37,21 @@
             </div>
         </div>
 
+        <main>
+            <section class="flex max-w-7xl mx-auto py-32">
+                <div class="space-y-8">
+                    <h2 class="text-5xl font-extrabold leading-16 w-full text-start font-mono max-w-9/12">Transforme sua presença digital. Comece a vender na internet com <br/><span class="text-primary">loja virtual</span></h2>
+                    <p class="text-lg leading-10 max-w-9/12">Seu site pode ser muito mais que uma vitrine: é um motor de vendas 24 horas, com design moderno, velocidade de carregamento ultrarrápida e estratégias que transformam visitantes em clientes. Pronto para escalar suas vendas online?</p>
+                    <div class="space-x-2">
+                        <a href="" class="rounded-md p-4 shadow-md hover:-translate-y-1.5 hover:duration-300 hover:cursor-pointer bg-gradient-to-tl from-[#d11414] to-primary text-white hover:outline-solid hover:outline-4 hover:outline-red-900 animate-pulse hover:animate-none">SOLICITE UM ORÇAMENTO GRÁTIS</a>
+                        <a href="" class="rounded-md p-4 text-primary hover:-translate-y-1.5 hover:cursor-pointer hover:outline-solid hover:outline-4 hover:outline-red-900">ME CONHEÇA UM POUCO MAIS</a>
+                    </div>
+                </div>
+
+                <img src="<?php echo e(asset("assets/image/image-hero.png")); ?>" alt="image hero"/>
+            </section>
+        </main>
+
         <footer class="border-t border-t-gray-300 py-8 text-center">
             EAP Projetos © 2025.
         </footer>
