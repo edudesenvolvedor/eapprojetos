a4406e84875e9a75b250c769459f80fda5d25a45
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-02-26 21:18:28 -0300

    [#56] Seção - Call to Action
    http://localhost:8080/work_packages/56

--- storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
+++ storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
@@ -122,6 +122,16 @@
                     </div>
                 </div>
             </section>
+
+            <section class="flex flex-col items-center max-w-7xl mx-auto space-y-16" data-aos="fade-right" data-aos-duration="500" data-aos-anchor-placement="center-center">
+                <div class="border-t border-gray-300 py-32 flex justify-center flex-wrap space-y-16 max-w-7xl mx-auto">
+                    <img src="https://picsum.photos/1024/720" alt="Mockup">
+                    <h2 class="text-5xl font-extrabold w-full text-center font-mono">Dê o Próximo Passo para o <span class="text-primary">Seu Site Ideal</span></h2>
+                    <p class="text-lg leading-10 text-center">Pronto para transformar a sua presença online?  Entre em contato conosco hoje mesmo para discutir o seu projeto de desenvolvimento web.  Solicite um orçamento sem compromisso e descubra como podemos ajudar a impulsionar o seu negócio para o sucesso digital.</p>
+                    <a href="/#form-budget" type="submit" class="rounded-md p-4 shadow-md hover:-translate-y-1.5 hover:duration-300 hover:cursor-pointer bg-gradient-to-tl from-[#d11414] to-primary text-white hover:outline-solid hover:outline-4 hover:outline-red-900 animate-pulse hover:animate-none">Quero Solicitar um Orçamento!</a>
+                </div>
+            </section>
+            
         </main>
 
         <footer class="border-t border-t-gray-300 py-8 text-center">
