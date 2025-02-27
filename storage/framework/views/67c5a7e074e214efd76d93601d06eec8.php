8e7f789bec3a40fd95e2550f9045a8fcba6a6564
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-02-26 21:16:41 -0300

    [#55] Seção - Serviços
    http://localhost:8080/work_packages/55

--- storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
+++ storage/framework/views/67c5a7e074e214efd76d93601d06eec8.php
@@ -59,7 +59,7 @@
                 </div>
             </section>
 
-            <section class="">
+            <section class="pb-64">
                 <hr class="max-w-7xl mx-auto mb-64 border-gray-300"/>
                 <div class="flex space-x-8">
                     <img src="https://picsum.photos/600/400" alt="Site Personalizado e Exclusivo" class="w-1/2" data-aos="fade-right" data-aos-delay="500" data-aos-anchor-placement="center-center"/>
@@ -80,6 +80,45 @@
                     <div class="max-w-3xl space-y-8 pt-16" data-aos="fade-left" data-aos-delay="500" data-aos-anchor-placement="center-center">
                         <h3 class="text-3xl font-extrabold w-full font-mono">Soluções Completas e Integradas</h3>
                         <p class="text-lg leading-10">Desde a criação do layout até a implementação de funcionalidades avançadas, oferecemos um serviço completo para que você não precise se preocupar com nada.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="flex flex-wrap max-w-7xl mx-auto py-32 items-center justify-around border-t border-gray-300">
+                    <span class="text-primary w-full text-center text-lg">Serviços</span>
+                    <h2 class="mb-16 w-full text-center text-5xl font-extrabold leading-16 font-mono max-w-9/12">O que eu faço</h2>
+                    <div class="grid grid-cols-3 gap-4 ">
+                        <div class="shadow-md bg-gradient-to-tl from-[#fff] to-white p-8 rounded-lg space-y-3">
+                            <img src="<?php echo e(asset("assets/image/icons/global-south-american.png")); ?>" alt="icon"/>
+                            <h3 class="text-3xl font-extrabold w-full font-mono">Title</h3>
+                            <p class="text-lg leading-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        </div>
+                        <div class="shadow-md bg-gradient-to-tl from-[#fff] to-white p-8 rounded-lg space-y-3">
+                            <img src="<?php echo e(asset("assets/image/icons/global-south-american.png")); ?>" alt="icon"/>
+                            <h3 class="text-3xl font-extrabold w-full font-mono">Title</h3>
+                            <p class="text-lg leading-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        </div>
+                        <div class="shadow-md bg-gradient-to-tl from-[#fff] to-white p-8 rounded-lg space-y-3">
+                            <img src="<?php echo e(asset("assets/image/icons/global-south-american.png")); ?>" alt="icon"/>
+                            <h3 class="text-3xl font-extrabold w-full font-mono">Title</h3>
+                            <p class="text-lg leading-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        </div>
+                        <div class="shadow-md bg-gradient-to-tl from-[#fff] to-white p-8 rounded-lg space-y-3">
+                            <img src="<?php echo e(asset("assets/image/icons/global-south-american.png")); ?>" alt="icon"/>
+                            <h3 class="text-3xl font-extrabold w-full font-mono">Title</h3>
+                            <p class="text-lg leading-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        </div>
+                        <div class="shadow-md bg-gradient-to-tl from-[#fff] to-white p-8 rounded-lg space-y-3">
+                            <img src="<?php echo e(asset("assets/image/icons/global-south-american.png")); ?>" alt="icon"/>
+                            <h3 class="text-3xl font-extrabold w-full font-mono">Title</h3>
+                            <p class="text-lg leading-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        </div>
+                        <div class="shadow-md bg-gradient-to-tl from-[#fff] to-white p-8 rounded-lg space-y-3">
+                            <img src="<?php echo e(asset("assets/image/icons/global-south-american.png")); ?>" alt="icon"/>
+                            <h3 class="text-3xl font-extrabold w-full font-mono">Title</h3>
+                            <p class="text-lg leading-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        </div>
                     </div>
                 </div>
             </section>
